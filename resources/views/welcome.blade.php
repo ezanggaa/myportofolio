607733544f4375627ecd1b566b16819bbca2f65b
Eza Angga Alfian <eza.anggaa@gmail.com>
2023-08-29 19:44:07 +0700

    First Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section id="home">
-        <div class="container ">
+        <div class="container min-100">
             <h5>Hello! I'am</h5>
             <h1 class="uppercase">Eza Angga Alfian</h1>
             <h5>Junior Web Developer</h5>
