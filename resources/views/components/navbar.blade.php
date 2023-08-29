9345953c1028edd8c0f5b69ea5593a3de5ad3e4b
Eza Angga Alfian <eza.anggaa@gmail.com>
2023-08-29 19:43:45 +0700

    First Commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-transparent">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('welcome') }}">
+            Eza<span class="text-primary">Anggaa</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#skills">Skills</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#portofolio">Portofolio</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <section id="home">
+        <div class="container ">
+            <h5>Hello! I'am</h5>
+            <h1 class="uppercase">Eza Angga Alfian</h1>
+            <h5>Junior Web Developer</h5>
+            <button class="btn btn-primary mt-3">My Resume</button>
+        </div>
+    </section>
+@endsection
